b16fc33c5012e2136ba865de8d53bb4f0e8a5ec6
Pakiza <esengazypakiza@gmail.com>
2023-02-15 14:19:27 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/boakwoon/winego/controllers"
+	"github.com/boakwoon/winego/database"
+	"github.com/boakwoon/winego/middleware"
+	"github.com/boakwoon/winego/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
