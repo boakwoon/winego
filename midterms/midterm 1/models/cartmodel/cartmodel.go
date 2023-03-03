7c396f41f19c534722e42b1e38e128268bde68c7
Pakiza <esengazypakiza@gmail.com>
2023-03-03 12:53:48 +0600

    fourth

---
+++ midterms/midterm 1/models/cartmodel/cartmodel.go
@@ -0,0 +1,25 @@
+package cartmodel
+
+import (
+	"github.com/boakwoon/winego/app"
+	"github.com/boakwoon/winego/config"
+)
+
+type CartModel struct {
+}
+
+func (*CartModel) FindCart(userId uint) (app.Cart, error) { // поиск корзины конкретного юзера
+
+	var cart app.Cart
+	rows, err := config.Init().Raw("select * from carts where user_id = ?", userId).Rows()
+	if err != nil {
+		return app.Cart{}, err
+	}
+	for rows.Next() {
+		rows.Scan(&cart.CartQuantity, &cart.Name, &cart.ProductId, &cart.Photo, &cart.Price,
+			&cart.Author, &cart.UserId)
+
+	}
+
+	return cart, nil
+}
